18a5de1fc42592ae328fa822dcbbe299889fb968
yehor-akunishnikov <faiteforever@gmail.com>
2024-11-18 09:54:36 +0200

    Added spy fn example

--- src/features/sort/Sort.test.tsx
+++ src/features/sort/Sort.test.tsx
@@ -1,8 +1,12 @@
-import {render, screen} from '@testing-library/react';
-import {describe, expect, it} from 'vitest';
+import {fireEvent, render, screen} from '@testing-library/react';
+import {afterEach, describe, expect, it, vi} from 'vitest';
 import {Sort} from './Sort.tsx';
 
 describe('Sort component test', () => {
+    afterEach(() => {
+        vi.restoreAllMocks()
+    });
+
     it('should render 2 buttons', () => {
         render(<Sort currentOption={null} setSortBy={() => {}}/>);
         const sortBtns = screen.getAllByTestId('sortBtn');
@@ -20,5 +24,19 @@ describe('Sort component test', () => {
         const sortBtns = screen.getAllByTestId('sortBtn');
         expect(sortBtns[0].className).toBe('bg-indigo-300 hover:bg-indigo-200 px-3 border border-black rounded');
         expect(sortBtns[1].className).toBe('hover:bg-indigo-300 px-3 border border-black rounded');
+    });
+    it('should set state on click', () => {
+        const setSortBySpy = vi.fn();
+
+        render(<Sort currentOption={null} setSortBy={setSortBySpy}/>);
+
+        const sortBtns = screen.getAllByTestId('sortBtn');
+
+        fireEvent.click(sortBtns[0]);
+        fireEvent.click(sortBtns[1]);
+
+        expect(setSortBySpy).toHaveBeenCalledTimes(2);
+        expect(setSortBySpy).toHaveBeenCalledWith('age');
+        expect(setSortBySpy).toHaveBeenCalledWith('name');
     });
 });
